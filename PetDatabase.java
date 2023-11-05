0e1f4d01e1776c2b7506f746d12faba7ede1c789
Avery Tribbett <apatribbett03@gmail.com>
2023-11-05 09:17:36 -0600

    Release 3: Program is fully functional and now includes all features. This release added in the update and remove features.

--- PetDatabase.java
+++ PetDatabase.java
@@ -7,10 +7,11 @@
  */
 
 /* * * * * * * * * *
- *    Release 2    *
+ *    Release 3    *
  * * * * * * * * * */
 
-/* Allow Users to add pets, view pets, search names and search ages */
+/* Allow Users to add pets, view pets, update pets,
+  remove pets, search names and search ages */
 
 import java.util.Scanner;
 import Pet.Pet;
@@ -28,51 +29,42 @@ public class PetDatabase {
     int userChoice;
     do {
       System.out.println();
+      // Because of error handling this will always be 1 - 7
       userChoice = getUserChoice();
 
       switch (userChoice) {
-        case 1 -> {
-          viewAllPets(); 
-          break;
-        }
-        case 2 -> {
-          addPets();
-          break;
-        }
-        case 3 -> {
-          searchPetsByName();
-          break;
-        }
-        case 4 -> {
-          searchPetsByAge();
-          break;
-        }
-        default -> {
-          System.out.println("Goodbye!");
-        }
+        case 1 : viewAllPets(); break;
+        case 2 : addPets(); break; 
+        case 3 : updatePet(); break;
+        case 4 : removePet(); break;
+        case 5 : searchPetsByName(); break;
+        case 6 : searchPetsByAge(); break;
+        default : System.out.println("Goodbye!"); 
       }
-    // Run while user does not enter 5
-    } while (userChoice != 5);
+    // Run while user does not enter 7
+    } while (userChoice != 7);
   }
 
   // Method to get the choice of the method the user wants to run.
   private static int getUserChoice() {
     int userChoice = 0;
-    // Make sure user enters 1 - 5
-    while (userChoice < 1 || userChoice > 5) {
+    // Make sure user enters 1 - 7
+    while (userChoice < 1 || userChoice > 7) {
       // Print options
       System.out.println("What would you like to do?");
       System.out.println(" 1) View all pets");
       System.out.println(" 2) Add more pets");
-      System.out.println(" 3) Search pets by name");
-      System.out.println(" 4) Search pets by age");
-      System.out.println(" 5) Exit program");
+      System.out.println(" 3) Update an existing pet");
+      System.out.println(" 4) Remove an existing pet");
+      System.out.println(" 5) Search pets by name");
+      System.out.println(" 6) Search pets by age");
+      System.out.println(" 7) Exit program");
       System.out.print("Your choice: ");
       userChoice = s.nextInt();
 
       // Print error handling message if they enter an invalid number
-      if (userChoice < 1 || userChoice > 5) {
-        System.out.println("Enter a number 1 - 5");
+      if (userChoice < 1 || userChoice > 7) {
+        System.out.println("Enter a number 1 - 7");
       }
       System.out.println();
     }
@@ -109,6 +101,44 @@ public class PetDatabase {
         }
       }
     }
+  }
+
+  // Method to change a pet 
+  private static void updatePet() {
+    // Update user on current pets available to update.
+    viewAllPets();
+
+    // Get Pet info 
+    System.out.print("Enter the pet ID you want to update: ");
+    int updateIndex = s.nextInt();
+    System.out.print("Enter new name and new age (name, age): ");
+    String newName = s.next();
+    int newAge = s.nextInt();
+
+    // Update pet 
+    pets[updateIndex].setAge(newAge);
+    pets[updateIndex].setName(newName);
+  }
+
+  // Method to remove an existing pet. 
+  private static void removePet() {
+    // Update the user on available pets to removee.
+    viewAllPets();
+
+    // Get index of pet to remove
+    System.out.print("Enter the pet ID to remove: ");
+    int delIndex = s.nextInt();
+
+    // Shift each item after the pet deleted to the left (effectively deleting it)
+    for (int i = delIndex; i < pets.length - 1; i++) {
+      pets[i] = pets[i + 1];
+    }
+    // Set the last item to null (we do this in case the database is full).
+    // Because if DB is full the final item wont get shifted.
+    pets[pets.length - 1] = null;
+
+    // Remove one from pet count.
+    petCount --;
   }
 
   // Method to search all pets and display only those with matching name.
--- README.md
+++ README.md
@@ -2,10 +2,15 @@
 
 ### Release 1 Features
 
-- View all pets.
+- View all Pets.
 - Add Pets
 
 ### Release 2 Features
 
 - Search Pets by name
 - Search Pets by age
+
+### Release 3 Features
+
+- Update Pet
+- Remove Pet
